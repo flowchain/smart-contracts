2bce5b5923ab74346c959030a7711856fd1259df
Jollen Chen <jollen@moko365.com>
2019-01-20 18:37:43 +0800

    update: EarlyTokenSale.sol

--- contracts/EarlyTokenSale.sol
+++ contracts/EarlyTokenSale.sol
@@ -1,9 +1,12 @@
-pragma solidity ^0.4.18;
+pragma solidity 0.4.25;
  
 /**
- * Copyright 2018, Flowchain.co
+ * Copyright 2018, The Flowchain Foundation Limited
  *
- * The FlowchainCoin (FLC) smart contract of private sale Round A
+ * The FlowchainCoin (FLC) Token Sale Contract
+ * 
+ *  - Private Sale A
+ *  - Monthly Vest
  */
 
 /**
@@ -29,119 +32,42 @@ library SafeMath {
 
         return c;
     }
-
-    /**
-    * @dev Multiplies two signed integers, reverts on overflow.
-    */
-    function mul(int256 a, int256 b) internal pure returns (int256) {
-        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-        if (a == 0) {
-            return 0;
-        }
-
-        require(!(a == -1 && b == INT256_MIN)); // This is the only case of overflow not detected by the check below
-
-        int256 c = a * b;
-        require(c / a == b);
-
-        return c;
-    }
-
-    /**
-    * @dev Integer division of two unsigned integers truncating the quotient, reverts on division by zero.
-    */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // Solidity only automatically asserts when dividing by 0
-        require(b > 0);
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-        return c;
-    }
-
-    /**
-    * @dev Integer division of two signed integers truncating the quotient, reverts on division by zero.
-    */
-    function div(int256 a, int256 b) internal pure returns (int256) {
-        require(b != 0); // Solidity only automatically asserts when dividing by 0
-        require(!(b == -1 && a == INT256_MIN)); // This is the only case of overflow
-
-        int256 c = a / b;
-
-        return c;
-    }
-
-    /**
-    * @dev Subtracts two unsigned integers, reverts on overflow (i.e. if subtrahend is greater than minuend).
-    */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b <= a);
-        uint256 c = a - b;
-
-        return c;
-    }
-
-    /**
-    * @dev Subtracts two signed integers, reverts on overflow.
-    */
-    function sub(int256 a, int256 b) internal pure returns (int256) {
-        int256 c = a - b;
-        require((b >= 0 && c <= a) || (b < 0 && c > a));
-
-        return c;
-    }
-
-    /**
-    * @dev Adds two unsigned integers, reverts on overflow.
-    */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a);
-
-        return c;
-    }
-
-    /**
-    * @dev Adds two signed integers, reverts on overflow.
-    */
-    function add(int256 a, int256 b) internal pure returns (int256) {
-        int256 c = a + b;
-        require((b >= 0 && c >= a) || (b < 0 && c < a));
-
-        return c;
-    }
-
-    /**
-    * @dev Divides two unsigned integers and returns the remainder (unsigned integer modulo),
-    * reverts when dividing by zero.
-    */
-    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b != 0);
-        return a % b;
-    }
 }
 
 interface Token {
+    /// @dev Mint an amount of tokens and transfer to the backer
+    /// @param to The address of the backer who will receive the tokens
+    /// @param amount The amount of rewarded tokens
+    /// @return The result of token transfer
     function mintToken(address to, uint amount) external returns (bool success);  
-    function setupMintableAddress(address _mintable) public returns (bool success);
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) public view returns (uint256 balance);
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) public returns (bool success);    
 }
 
 contract MintableSale {
     // @notice Create a new mintable sale
+    /// @param vestingAddress The vesting app    
     /// @param rate The exchange rate
     /// @param fundingGoalInEthers The funding goal in ethers
     /// @param durationInMinutes The duration of the sale in minutes
     /// @return 
-    function createMintableSale(uint256 rate, uint256 fundingGoalInEthers, uint durationInMinutes) external returns (bool success);
+    function createMintableSale(address vestingAddress, uint256 rate, uint256 fundingGoalInEthers, uint durationInMinutes) public returns (bool success);
 }
 
-contract EarlyTokenSale is MintableSale {
+contract VestingTokenSale is MintableSale {
     using SafeMath for uint256;
     uint256 public fundingGoal;
     uint256 public tokensPerEther;
     uint public deadline;
+    uint public tick;
     address public multiSigWallet;
     uint256 public amountRaised;
     Token public tokenReward;
@@ -157,39 +83,56 @@ contract EarlyTokenSale is MintableSale {
 
     event FundTransfer(address backer, uint amount);
 
+    address public addressOfVestingApp;
+    uint256 constant public   VESTING_DURATION    =  31536000; // 1 Year in second
+    uint256 constant public   CLIFF_DURATION      =   2592000; // 1 months (30 days) in second
+
     /* Constrctor function */
-    function EarlyTokenSale(
+    function VestingTokenSale(
         address _addressOfTokenUsedAsReward
     ) payable {
         creator = msg.sender;
         multiSigWallet = 0x9581973c54fce63d0f5c4c706020028af20ff723;
+
         // Token Contract
         addressOfTokenUsedAsReward = _addressOfTokenUsedAsReward;
         tokenReward = Token(addressOfTokenUsedAsReward);
+
         // Setup accredited investors
         setupAccreditedAddress(0xec7210E3db72651Ca21DA35309A20561a6F374dd, 1000);
+
+        // Initialize the ticker
+        tick = now;
     }
 
     // @dev Start a new mintable sale.
+    // @param vestingAddress The vesting app
     // @param rate The exchange rate in ether, for example 1 ETH = 6400 FLC
     // @param fundingGoalInEthers
     // @param durationInMinutes
-    function createMintableSale(uint256 rate, uint256 fundingGoalInEthers, uint durationInMinutes) external returns (bool success) {
+    function createMintableSale(address vestingAddrss, uint256 rate, uint256 fundingGoalInEthers, uint durationInMinutes) public returns (bool success) {
         require(msg.sender == creator);
         require(isFunding == false);
         require(rate <= 6400 && rate >= 1);                   // rate must be between 1 and 6400
-        require(fundingGoalInEthers >= 1000);        
+        require(fundingGoalInEthers >= 1);        
         require(durationInMinutes >= 60 minutes);
 
-        deadline = now + durationInMinutes * 1 minutes;
+        addressOfVestingApp = vestingAddrss;
+
+        // increase the tick
+        if (now > tick) {
+            tick = now;
+        }
+
+        deadline = tick + durationInMinutes * 1 minutes;
         fundingGoal = amountRaised + fundingGoalInEthers * 1 ether;
         tokensPerEther = rate;
         isFunding = true;
         return true;    
     }
 
-    modifier afterDeadline() { if (now > deadline) _; }
-    modifier beforeDeadline() { if (now <= deadline) _; }
+    modifier afterDeadline() { if (now > tick && now > deadline) _; }
+    modifier beforeDeadline() { if (now > tick && now <= deadline) _; }
 
     /// @param _accredited The address of the accredited investor
     /// @param _amountInEthers The amount of remaining ethers allowed to invested
@@ -238,7 +181,7 @@ contract EarlyTokenSale is MintableSale {
         uint256 amount = msg.value;        
         require(amount >= 1 ether);
 
-        require(accredited[msg.sender] - amount >= 0); 
+        require(accredited[msg.sender] >= amount);
 
         multiSigWallet.transfer(amount);      
         balanceOf[msg.sender] += amount;
@@ -246,7 +189,10 @@ contract EarlyTokenSale is MintableSale {
         amountRaised += amount;
         FundTransfer(msg.sender, amount);
 
-        uint256 value = amount.mul(tokensPerEther);        
-        tokenReward.mintToken(msg.sender, value);        
-    }
+        // total releasable tokens
+        uint256 value = amount.mul(tokensPerEther);
+
+        // Mint tokens and keep it in the contract
+        tokenReward.mintToken(addressOfVestingApp, value);
+    }   
 }
